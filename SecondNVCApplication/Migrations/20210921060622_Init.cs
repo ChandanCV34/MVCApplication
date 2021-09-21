49eee8ebc0fa1b0d70d9ff2ade0acb9df5e78d95
unknown <Administrator@KANINI-LTP-456.gisbizdev.gisbiz.chennai.com>
2021-09-21 20:03:55 +0530

    MVCApplication

---
+++ SecondNVCApplication/Migrations/20210921060622_Init.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SecondNVCApplication.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "departments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_departments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_employees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_employees_departments_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalTable: "departments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "departments",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 101, "Admin" });
+
+            migrationBuilder.InsertData(
+                table: "employees",
+                columns: new[] { "Id", "Age", "DepartmentId", "Name" },
+                values: new object[] { 1, 21, 101, "Tim" });
+
+            migrationBuilder.InsertData(
+                table: "employees",
+                columns: new[] { "Id", "Age", "DepartmentId", "Name" },
+                values: new object[] { 2, 31, 101, "jim" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_employees_DepartmentId",
+                table: "employees",
+                column: "DepartmentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "employees");
+
+            migrationBuilder.DropTable(
+                name: "departments");
+        }
+    }
+}
---
+++ SecondNVCApplication/Models/CompanyContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondNVCApplication.Models
+{
+    public class CompanyContext : DbContext
+    {
+        public CompanyContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> employees { get; set; }
+        public DbSet<Department> departments { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>().HasData(
+                new Department() { Id = 101, Name = "Admin" }
+                );
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee() { Id = 1, Name = "Tim", Age = 21, DepartmentId = 101 },
+                new Employee() { Id = 2, Name = "jim", Age = 31, DepartmentId = 101 }
+                );
+        }
+    }
+}
